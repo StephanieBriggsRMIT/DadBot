5f1fb7fe02035660f51a30a6b548e01ade54369c
s3383506 <66418584+StephanieBriggsRMIT@users.noreply.github.com>
2021-05-08 18:28:20 +1000

    added modes

--- src/dadbot.js
+++ src/dadbot.js
@@ -1,12 +1,16 @@
-const { Client } = require('discord.js');
+const { Client, Message } = require('discord.js');
 require('dotenv').config()
 
 const token = process.env.DISCORDJS_BOT_TOKEN;
 const bot = new Client();
+const prefix = '-';
 
 // Make sure the below arrays are equal length!
 const questions = ['Are we there yet?', 'I\'m hungry', 'Whats for dinner?']
-const answers = ['Nearly!', 'Hi Hungry \', I\'m dad!', 'Dirt!']
+const answers1 = ['Nearly!', 'Hi Hungry \', I\'m dad!', 'Dirt!']
+const answers2 = ['What a silly question', 'Not this again!', 'How would I know']
+
+var mode;
 
 bot.login(token);
 bot.on('ready', () => {
@@ -15,10 +19,26 @@ bot.on('ready', () => {
 
 bot.on('message', msg => {
 
-    for (let i = 0; i < questions.length; i++) {
-        if (msg.content === questions[i]) {
-            msg.reply(answers[i])
-        }
+    if (msg.content.startswith(prefix)) {
+
+        const args = msg.content.slice(prefix.length).split(/ +/);
+        const command = args.shift().toLowerCase;
+
+        mode = command;
     }
 
+    if (mode === 1) {
+        for (let i = 0; i < questions.length; i++) {
+            if (msg.content === questions[i]) {
+                msg.reply(answers1[i])
+            }
+        }
+    }
+    else if (mode === 2) {
+        for (let i = 0; i < questions.length; i++) {
+            if (msg.content === questions[i]) {
+                msg.reply(answers2[i])
+            }
+        }
+    }
 });
