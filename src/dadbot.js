5f665bb87aa0c30a300604d55cc6f300603e56b1
s3383506 <66418584+StephanieBriggsRMIT@users.noreply.github.com>
2021-05-09 15:43:41 +1000

    Update with errors corrected for user changing mode as command

--- src/dadbot.js
+++ src/dadbot.js
@@ -3,13 +3,14 @@ require('dotenv').config()
 
 const token = process.env.DISCORDJS_BOT_TOKEN;
 const bot = new Client();
-const prefix = '-';
+const prefix = '/mode';
 
 // Make sure the below arrays are equal length!
 const questions = ['Are we there yet?', 'I\'m hungry', 'Whats for dinner?']
 const answers1 = ['Nearly!', 'Hi Hungry \', I\'m dad!', 'Dirt!']
-const answers2 = ['What a silly question', 'Not this again!', 'How would I know']
+const answers2 = ['Not this again', 'You will have to wait!', 'Nothing if you keep asking!']
 
+let mode = 1;
 
 bot.login(token);
 bot.on('ready', () => {
@@ -18,37 +19,30 @@ bot.on('ready', () => {
 
 bot.on('message', msg => {
 
-    var mode= '';
+    if (msg.content.startsWith(prefix)) {
 
-    if (msg === 'set mode two') {
-        mode= 'two'
-        console.log('mode')
-        msg.reply('set to mode two')
-        return
+        const args = msg.content.split(" ");
+        const command = args[1]
+
+        mode = parseInt(command);
+        msg.reply('Changed to mode: ' + mode)
+
     }
-   /* if (msg.content.startsWith(prefix)) {
 
-        const args = msg.content.slice(prefix.length).split(/ +/);
-        const command = args.shift().toLowerCase;
-
-        mode = command;
-    
-   } */
-
-    if (mode === 'one') {
+    if (mode === 1) {
         for (let i = 0; i < questions.length; i++) {
             if (msg.content === questions[i]) {
                 msg.reply(answers1[i])
             }
         }
     }
-    else if (mode === 'two') {
+    else if (mode === 2) {
         for (let i = 0; i < questions.length; i++) {
             if (msg.content === questions[i]) {
                 msg.reply(answers2[i])
             }
         }
     }
-   
+
 
 });
