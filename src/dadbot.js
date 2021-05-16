af4e68c987a64f74473c06c2b3e2ee05302be484
Sam Ashton <sam.ashton@hostking.com.au>
2021-05-16 13:18:10 +1000

    Added new addQuestion function

--- src/dadbot.js
+++ src/dadbot.js
@@ -38,6 +38,7 @@ const answers2 =
 const modename =
     ['Normal',
         'Annoyed']
+ 
 
 // Login to Discord
 bot.login(token);
@@ -53,17 +54,24 @@ bot.on('message', msg => {
         const args = msg.content.split(" ");
         const command = args[1]
 
-        mode = parseInt(command);
-        if (mode <= modename.length) {
-            msg.reply('Changed to mode: ' + modename[mode - 1])
+        if (command === "add") { 
+            // Send the Message Content to the addQuestion function and post its response
+            msg.reply(addQuestion(msg.content));
         }
         else {
-            let messagereply = 'Invalid mode, please enter from options: \n';
-
-            for (let i = 0; i < modename.length; i++) {
-                messagereply = messagereply + (i + 1) + ' - ' + modename[i] + '\n';
+            mode = parseInt(command);
+            if (mode <= modename.length) {
+                msg.reply('Changed to mode: ' + modename[mode - 1])
             }
-            msg.reply(messagereply)
+            else {
+                let messagereply = 'Invalid mode, please enter from options: \n';
+
+                for (let i = 0; i < modename.length; i++) {
+                    messagereply = messagereply + (i + 1) + ' - ' + modename[i] + '\n';
+                }
+                messagereply = messagereply + 'add - Add a new Question and Answer';
+                msg.reply(messagereply)
+            }
         }
     }
 
@@ -84,5 +92,32 @@ bot.on('message', msg => {
     }
 
 
-
 });
+
+// Function to Add new questions to the Arrays
+
+function addQuestion(message) {
+    let response = '';
+    
+    // First break the original message up to get the Question and answer after the /mode add
+    let str = message.slice(10, message.length);
+    let strArgs = str.split("\" \"");
+
+    // Check for 2 arguments only add the question if two exist
+    if (strArgs.length != 2) {
+        response = 'To add a new Question, enter the following\n /mode add \"new question\" \'new answer\' ';
+    } else {
+        let newQuestion = strArgs[0].slice(1, strArgs[0].length);
+        let newAnswer = strArgs[1].slice(0, strArgs[1].length - 1);
+
+        questions.push(newQuestion);
+        answers1.push(newAnswer);
+        answers2.push(newAnswer);
+
+        response = 'The following has been added: \n Question: ' + newQuestion + '\nAnswer: ' + newAnswer;
+    }
+
+    return response;
+    
+
+}
